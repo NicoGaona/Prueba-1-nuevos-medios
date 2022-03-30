876c2834cd56ffaaf8669be888bd4c8b3e31f212
Nicolás <101114664+NicoGaona@users.noreply.github.com>
2022-03-30 01:49:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var elEncabezado = document.querySelector("header");
+var elColor, campeon;
+
+function preload() {
+    campeon = loadJSON("https://github.com/NicoGaona/json/blob/main/monos.json");
+}
+
+function setup() {
+    var lasOpciones = [];
+    Object.values(campeon).forEach((p) => {
+  if(p.name){
+     lasOpciones.push(p.img)
+   }});
+   console.log(lasOpciones);
+      createElement("h1", "¿Podrías dibujar a esta leyenda?").parent(elEncabezado).id("title");
+      createCanvas(windowWidth, windowHeight).position(0, 0).style("z-index", -1);
+      createImg(random(lasOpciones),"holi").addClass("chiqui");
+      background("#000000");
+  }
